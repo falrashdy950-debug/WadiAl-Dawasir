741f016affea151efb815eeca4063f5675500513
falrashdy950-debug <falrashdy950@gmail.com>
2026-04-02 21:05:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// 1. عرض رسالة ترحيبية (تفاعل JavaScript)
 function welcomeMessage() {
-    alert("حياك الله في وادي الدواسر! نورت صفحتنا.");
-}
-
-// 2. قائمة Hamburger للشاشات الصغيرة
-function toggleMenu() {
-    var menu = document.getElementById("navlinks");
-    menu.classList.toggle("show");
-}
-
-// 4. تحسين تجربة المستخدم (تفاعل عند تمرير الفأرة)
-function changeText() {
-    document.getElementById("hover-box").innerHTML = "وادي الدواسر قلب نجد النابض! ✨";
-}
-function resetText() {
-    document.getElementById("hover-box").innerHTML = "مرر الفأرة هنا لاكتشاف سر!";
+    alert("أهلاً بكِ في مشروعي عن وادي الدواسر.. اتمنى ان يعجبك!");
 }
